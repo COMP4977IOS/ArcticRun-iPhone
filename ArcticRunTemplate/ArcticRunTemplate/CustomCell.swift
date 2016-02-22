362e38b3cb6098ed4d8ecc33a42ae1db66508398
MaThAiS <wiser.man1@gmail.com>
2016-02-21 19:23:00 -0800

    Added tab components

---
+++ ArcticRunTemplate/ArcticRunTemplate/CustomCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomCell.swift
+//  ArcticRunTemplate
+//
+//  Created by Matt Wiseman on 2016-02-21.
+//  Copyright © 2016 Matt Wiseman. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var Status: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
