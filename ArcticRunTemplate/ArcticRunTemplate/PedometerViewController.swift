c96a36dbccf8cb0fdcf071cfd1f1796dfefa4210
MaThAiS <wiser.man1@gmail.com>
2016-02-21 18:44:33 -0800

    Full starting template

---
+++ ArcticRunTemplate/ArcticRunTemplate/PedometerViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PedometerViewController.swift
+//  ArcticRunTemplate
+//
+//  Created by Matt Wiseman on 2016-02-21.
+//  Copyright © 2016 Matt Wiseman. All rights reserved.
+//
+
+import UIKit
+
+class PedometerViewController: UIViewController {
+    
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (self.revealViewController() != nil) {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    /*
+    // MARK: Navigation -
+    
+    // In a storyboard-based application,you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController
+    // Pass the selected object to the new view controller
+    }
+    */
+    
+    
+    
+    
+}
