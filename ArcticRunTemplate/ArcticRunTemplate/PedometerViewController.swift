d2d7b63153a451a94c774b858a2cbd196360ea34
Kpatena <kamilpatena1221@gmail.com>
2016-03-02 23:08:53 -0800

    Added pedometer for mission page and healthkit authorization and reading steps and distance from midnight of today to right now

--- ArcticRunTemplate/ArcticRunTemplate/PedometerViewController.swift
+++ ArcticRunTemplate/ArcticRunTemplate/PedometerViewController.swift
@@ -2,19 +2,31 @@
 //  PedometerViewController.swift
 //  ArcticRunTemplate
 //
-//  Created by Matt Wiseman on 2016-02-21.
-//  Copyright © 2016 Matt Wiseman. All rights reserved.
 //
 
 import UIKit
-
+import CoreMotion
 class PedometerViewController: UIViewController {
     
+    let pedoMeter = CMPedometer()
+    var timer = NSTimer()
+    var rightNow:NSDate?
+    var testNumber:Int = 25;
+    var testSteps:Int = 0;
     
     @IBOutlet weak var menuButton: UIBarButtonItem!
     
+    @IBOutlet weak var stepsTextForDay: UILabel!
+    @IBOutlet weak var missionStateLabel: UILabel!
+    
+    @IBOutlet weak var numberOfStepsText: UILabel!
+    @IBOutlet weak var outOfText: UILabel!
+    
+    @IBOutlet weak var distanceTravelledTodayText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        outOfText.text = String(testNumber)
         // Do any additional setup after loading the view, typically from a nib.
         
         if (self.revealViewController() != nil) {
@@ -22,17 +34,60 @@ class PedometerViewController: UIViewController {
             menuButton.action = "revealToggle:"
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        //Call HealthKitHelper to get Steps and Distance for today
+        //Need now to work on storing this data onto cloudkit
+        HealthKitHelper().recentSteps() { steps, error in
+           self.stepsTextForDay.text = String(steps)
+        }
+        
+        HealthKitHelper().recentDistance() { distance, error in
+            self.distanceTravelledTodayText.text = String(distance)
+        }
+        
     }
     
-    /*
-    // MARK: Navigation -
-    
-    // In a storyboard-based application,you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController
-    // Pass the selected object to the new view controller
+    @IBAction func start(sender : AnyObject) {
+        rightNow = NSDate()
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.75, target: self, selector: Selector("updateCounting"), userInfo: nil, repeats: true)
     }
-    */
+    
+    //Counting number of steps taken 
+    //Test steps is just incrementing change it to being the number of steps from pedometer
+    func updateCounting() {
+        testSteps++
+        self.numberOfStepsText.text = String(testSteps)
+        if (testSteps >= self.testNumber) {
+            self.completeMission()
+        }
+        
+        if(CMPedometer.isStepCountingAvailable()){
+            self.pedoMeter.queryPedometerDataFromDate(rightNow!, toDate: NSDate()) { (data : CMPedometerData?, error) -> Void in
+                print(data)
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if(error == nil){
+                        if (data!.numberOfSteps as Int >= self.testNumber) {
+                            self.completeMission()
+                        }
+                        self.numberOfStepsText.text = "\(data!.numberOfSteps)"
+                    }
+                })
+                
+            }
+        }
+    }
+    
+    //When the mission is complete do something, here you can add that it finished in database
+    func completeMission() {
+        missionStateLabel.text = "Mission Complete!"
+        timer.invalidate()
+    }
+    
+    //Halts run and turns of timer that runs pedometer
+    @IBAction func stopRun() {
+        timer.invalidate()
+    }
+
     
     
     
