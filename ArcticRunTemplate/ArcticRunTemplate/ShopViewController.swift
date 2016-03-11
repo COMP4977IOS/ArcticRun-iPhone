910b5c5488be645acda4c3851a3cf7c838dac8bc
arthur pankiewicz <arthurpankiewicz@gmail.com>
2016-03-11 14:12:45 -0800

    shop changes

--- ArcticRunTemplate/ArcticRunTemplate/ShopViewController.swift
+++ ArcticRunTemplate/ArcticRunTemplate/ShopViewController.swift
@@ -13,8 +13,12 @@ private let reuseIdentifier = "itemCell"
 class ShopViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var menuButton: UIBarButtonItem!
-    @IBOutlet weak var currency1: UILabel!
-    @IBOutlet weak var currency2: UILabel!
+
+    @IBOutlet weak var currency1: UIButton!
+    @IBOutlet weak var currency2: UIButton!
+    @IBOutlet weak var currency1Label: UILabel!
+    @IBOutlet weak var currency2Label: UILabel!
+    var useCur1 = true
     
     var tableData: [String] = ["500", "1000", "2000", "3000", "4000", "5000", "10000", "12500",
         "15000", "17500", "20000", "25000", "30000", "35000", "40000", "45000", "50000", "60000", "70000", "80000", "90000", "100000"]
@@ -22,18 +26,44 @@ class ShopViewController: UIViewController, UICollectionViewDataSource, UICollec
         "qmitem.png", "qmitem.png", "qmitem.png", "qmitem.png", "qmitem.png", "qmitem.png", "qmitem.png", "qmitem.png",
         "qmitem.png", "qmitem.png", "qmitem.png", "qmitem.png", "qmitem.png", "qmitem.png", "qmitem.png", "qmitem.png", ]
     
+    let screenSize = UIScreen.mainScreen().bounds
+    
     
     override func viewDidLoad() {
-        
-        currency1.text = "10000"
+
+        currency1Label.text = "5000"
+        currency2Label.text = "15000"
+        currency1Label.textColor = UIColor.redColor()
+        currency2Label.textColor = UIColor.blueColor()
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+//        let screenWidth = screenSize.width
+//        layout.itemSize = CGSize(width: screenWidth/3, height: screenWidth/3)
+//        layout.minimumInteritemSpacing = 0
+//        layout.minimumLineSpacing = 0
         
         if (self.revealViewController() != nil) {
             menuButton.target = self.revealViewController()
             menuButton.action = "revealToggle:"
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    @IBAction func useCurrency1(sender: AnyObject) {
+        if useCur1 == false {
+            useCur1 = true
+            currency1Label.textColor = UIColor.redColor()
+            currency2Label.textColor = UIColor.blueColor()
+        }
+    }
+    
+    @IBAction func useCurrency2(sender: AnyObject) {
+        if useCur1 == true {
+            useCur1 = false
+            currency1Label.textColor = UIColor.blueColor()
+            currency2Label.textColor = UIColor.redColor()
         }
     }
     
@@ -43,7 +73,7 @@ class ShopViewController: UIViewController, UICollectionViewDataSource, UICollec
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell: colvwCell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! colvwCell
-        
+
         cell.lblCell.text = tableData[indexPath.row]
         let image = UIImage(named: tableImages[indexPath.row])
         cell.imgCell.image = image
@@ -52,34 +82,65 @@ class ShopViewController: UIViewController, UICollectionViewDataSource, UICollec
         return cell
     }
     
+//    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+//        let screenWidth = screenSize.width
+//        return CGSize(width: screenWidth/3, height: screenWidth/3);
+//        
+//    }
+    
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         let priceString = tableData[indexPath.row]
         let price = Int(priceString)
-        let totalString = currency1.text
-        let total = Int(totalString!)
         
-        if(price > total) {
-            let alertController = UIAlertController(title: "Item \(indexPath.row + 1)", message: "You do not have enough points.", preferredStyle: .Alert)
-            let okAction = UIAlertAction(title: "OK", style: .Destructive) { (action) -> Void in print("test") }
-            alertController.addAction(okAction)
-            self.presentViewController(alertController, animated: true, completion: nil)
-        } else {
-            let alertController = UIAlertController(title: "Item \(indexPath.row + 1)", message: "Purchase?", preferredStyle:
+        if useCur1 == true {
+            let totalString = currency1Label.text
+            let total = Int(totalString!)
+
+            if(price > total) {
+                let alertController = UIAlertController(title: "Item \(indexPath.row + 1)", message: "You do not have enough points.", preferredStyle: .Alert)
+                let okAction = UIAlertAction(title: "OK", style: .Destructive) { (action) -> Void in print("test") }
+                alertController.addAction(okAction)
+                self.presentViewController(alertController, animated: true, completion: nil)
+            } else {
+                let alertController = UIAlertController(title: "Item \(indexPath.row + 1)", message: "Purchase?", preferredStyle:
                 .ActionSheet)
-            let party1 = UIAlertAction(title: "Party Member 1", style: .Default, handler: { action in self.currency1.text = String(total! - price!) } )
-            let party2 = UIAlertAction(title: "Party Member 2", style: .Default, handler: { action in self.currency1.text = String(total! - price!) } )
-            let party3 = UIAlertAction(title: "Party Member 3", style: .Default, handler: { action in self.currency1.text = String(total! - price!) } )
-            let party4 = UIAlertAction(title: "Party Member 4", style: .Default, handler: { action in self.currency1.text = String(total! - price!) } )
-            alertController.addAction(party1)
-            alertController.addAction(party2)
-            alertController.addAction(party3)
-            alertController.addAction(party4)
-            self.presentViewController(alertController, animated: true, completion: nil)
-
+                let party1 = UIAlertAction(title: "Party Member 1", style: .Default, handler: { action in self.currency1Label.text = String(total! - price!) } )
+                let party2 = UIAlertAction(title: "Party Member 2", style: .Default, handler: { action in self.currency1Label.text = String(total! - price!) } )
+                let party3 = UIAlertAction(title: "Party Member 3", style: .Default, handler: { action in self.currency1Label.text = String(total! - price!) } )
+                let party4 = UIAlertAction(title: "Party Member 4", style: .Default, handler: { action in self.currency1Label.text = String(total! - price!) } )
+                let cancel = UIAlertAction(title: "Cancel", style: .Destructive) { (action) -> Void in print("cancelled") }
+                alertController.addAction(party1)
+                alertController.addAction(party2)
+                alertController.addAction(party3)
+                alertController.addAction(party4)
+                alertController.addAction(cancel)
+                self.presentViewController(alertController, animated: true, completion: nil)
+            }
+        } else {
+            let totalString = currency2Label.text
+            let total = Int(totalString!)
+            
+            if(price > total) {
+                let alertController = UIAlertController(title: "Item \(indexPath.row + 1)", message: "You do not have enough points.", preferredStyle: .Alert)
+                let okAction = UIAlertAction(title: "OK", style: .Destructive) { (action) -> Void in print("test") }
+                alertController.addAction(okAction)
+                self.presentViewController(alertController, animated: true, completion: nil)
+            } else {
+                let alertController = UIAlertController(title: "Item \(indexPath.row + 1)", message: "Purchase?", preferredStyle:
+                    .ActionSheet)
+                let party1 = UIAlertAction(title: "Party Member 1", style: .Default, handler: { action in self.currency2Label.text = String(total! - price!) } )
+                let party2 = UIAlertAction(title: "Party Member 2", style: .Default, handler: { action in self.currency2Label.text = String(total! - price!) } )
+                let party3 = UIAlertAction(title: "Party Member 3", style: .Default, handler: { action in self.currency2Label.text = String(total! - price!) } )
+                let party4 = UIAlertAction(title: "Party Member 4", style: .Default, handler: { action in self.currency2Label.text = String(total! - price!) } )
+                let cancel = UIAlertAction(title: "Cancel", style: .Destructive) { (action) -> Void in print("cancelled") }
+                alertController.addAction(party1)
+                alertController.addAction(party2)
+                alertController.addAction(party3)
+                alertController.addAction(party4)
+                alertController.addAction(cancel)
+                self.presentViewController(alertController, animated: true, completion: nil)
+            }
         }
-        
-        
-
     }
     
 
