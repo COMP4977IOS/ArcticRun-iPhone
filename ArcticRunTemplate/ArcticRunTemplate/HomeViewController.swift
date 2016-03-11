a30d36b8cb3a16389ba6a5d04e1e028ec45a49ad
Jan Ycasas <ycasas.jan@gmail.com>
2016-03-11 10:28:27 -0800

    Temp

--- ArcticRunTemplate/ArcticRunTemplate/HomeViewController.swift
+++ ArcticRunTemplate/ArcticRunTemplate/HomeViewController.swift
@@ -20,6 +20,11 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var menuButton: UIBarButtonItem!
     
+    @IBOutlet weak var timeLabel: UILabel!
+    var timer = NSTimer()
+    @IBOutlet weak var startButton: UIButton!
+    var startTime = NSTimeInterval()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -81,6 +86,58 @@ class HomeViewController: UIViewController {
     }
     
     
+    @IBAction func startFunction(sender: AnyObject) {
+        if !timer.valid {
+            let aSelector : Selector = "updateTime"
+            if(startButton.currentTitle == "Start" || startButton.currentTitle == "Resume"){
+                timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector,     userInfo: nil, repeats: true)
+                startTime = NSDate.timeIntervalSinceReferenceDate()
+                startButton.setTitle("Pause",forState: .Normal)
+            }
+        }
+        
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        timer.invalidate()
+        timer = NSTimer()
+        startButton.setTitle("Start",forState: .Normal)
+    }
+    
+    func updateTime(){
+        let currentTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        //Find the difference between current time and start time.
+        
+        var elapsedTime: NSTimeInterval = currentTime - startTime
+        
+        //calculate the minutes in elapsed time.
+        
+        let minutes = UInt8(elapsedTime / 60.0)
+        
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        
+        let seconds = UInt8(elapsedTime)
+        
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        
+        let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strFraction = String(format: "%02d", fraction)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        
+        timeLabel.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+    }
+
 }
 
 
