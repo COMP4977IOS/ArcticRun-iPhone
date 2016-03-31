91697f5d5d669656a0156bd1f0d47c9a57eb0712
Matt Wiseman <a00897119@se12l0303-s26.wifi.bcit.ca>
2016-03-30 23:51:16 -0700

    Attempted at a timer, after pause resets, the only bug

--- ArcticRunTemplate/PlayScreenViewController.swift
+++ ArcticRunTemplate/PlayScreenViewController.swift
@@ -15,20 +15,27 @@ class PlayScreenViewController : UIViewController {
     @IBOutlet weak var stopBtn: UIButton!
     var passData : String!
     
+    @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
+    
+    var startTimer: Bool!
+    var strMinutes : String!
+    var strSeconds : String!
     
     let imagePause = UIImage(named:"Pause") as UIImage?
     let imagePlay = UIImage(named: "Play") as UIImage?
     let imageStop = UIImage(named: "Stop") as UIImage?
+    
+    var startTime = NSTimeInterval()
+    var referTime = NSTimeInterval()
+    var timer = NSTimer()
+    
     
     var running = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadBG()
-        playBtn.setImage(imagePlay, forState: UIControlState.Normal)
-        stopBtn.setImage(imageStop, forState: UIControlState.Normal)
-        playGame()
         
         // TODO: Pass data from MissionViewController to label, not working like it is from GameMap
         //titleLabel.text = passData
@@ -36,35 +43,26 @@ class PlayScreenViewController : UIViewController {
         titleLabel.text = "Passed Label Data"
         
         
-        
     }
     
     
-    func playGame() {
-        
-        playBtn.setImage(imagePlay, forState: UIControlState.Normal)
-        running = true
-        
-    }
     
-    func pause() {
+    @IBAction func pause(sender: AnyObject) {
         
-        playBtn.setImage(imagePause, forState: UIControlState.Normal)
+        timer.invalidate()
         running = false
         
-        
     }
-    
     @IBAction func playAndPause(sender: AnyObject) {
         
-        if(running == false) {
-            
-            return playGame()
-            
-        }else{
-            
-            return pause()
-            
+        let aSelector : Selector = "updateCounter"
+        
+        if !running {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+            startTime = NSDate.timeIntervalSinceReferenceDate()
+            running = true
+        }else {
+           
         }
     }
     
@@ -72,6 +70,7 @@ class PlayScreenViewController : UIViewController {
         let alert = UIAlertController(title: "The Ship", message: "Are you sure you want to quit?", preferredStyle: UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.Cancel, handler: nil))
         alert.addAction(UIAlertAction(title: "yes", style: UIAlertActionStyle.Default, handler: { dismiss }()))
+        
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
@@ -84,9 +83,29 @@ class PlayScreenViewController : UIViewController {
         
     }
     
+    func updateCounter() {
+        
+        let currentTime = NSDate.timeIntervalSinceReferenceDate()
+        var elapsedTime: NSTimeInterval = currentTime - startTime
+        
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        
+        timerLabel.text = "\(strMinutes):\(strSeconds)"
+        
+    }
     
     @IBAction func dismiss(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
+        timer.invalidate()
+        timerLabel.text = "\(00):\(00)"
+        running = false
     }
     
 }
