7d9498bc6f009e358f85f3a3402e33b9dc219bad
Jox <joxched@gmail.com>
2016-04-03 22:05:10 -0700

    fix merge isssues

--- ArcticRunTemplate/ArcticRunTemplate/CalorieCell.swift
+++ ArcticRunTemplate/ArcticRunTemplate/CalorieCell.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 class CalorieCell: UITableViewCell {
-   
+    
     @IBOutlet weak var calorieCell: UILabel!
-
+    
     @IBOutlet weak var date: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
